ce4c1b750133a36b6106679334a61248d9ba4a21
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-10-17 13:59:00 -0400

    huh

--- index.js
+++ index.js
@@ -8,8 +8,31 @@ const DOMSelectors = {
   form: document.querySelector(".form"),
   container: document.querySelector(".container"),
   card: document.querySelector(".card"),
+
+  NameInput: document.getELementsByID("name-input"),
+  DescInput: document.getELementsByID("Desc-input"),
+  ImageInput: document.getELementsByID("Image-input"),
+  ColorInput: document.getELementsByID("Color-input"),
 };
 console.log(DOMSelectors.text);
+
+function makecard(event) {
+  event.preventDefault();
+  let name = DOMSelectors.NameInput.value;
+  let desc = DOMSelectors.DescInput.value;
+  let image = DOMSelectors.ImageInput.value;
+  let color = DOMSelectors.ColorInput.value;
+
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class = "card">
+    <h2 class = "card-header"> ${name} </h2>
+    <img src"${image}" alt="image made" class = "card-image">
+    
+    `
+  );
+}
 
 /*DOMSelectors.button.addEventListener("click", function (event) {
   console.log(event.target.parentElement);
