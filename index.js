aac2504e16ff1af4d8cb50a9500f39ffcaff4e3a
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-10-18 12:35:46 -0400

    piplup

--- index.js
+++ index.js
@@ -7,40 +7,38 @@ const DOMSelectors = {
   button: document.querySelector(".btn"),
   form: document.querySelector(".form"),
   container: document.querySelector(".container"),
-  
 
-  NameInput: document.getElementsByID("name-input"),
-  DescInput: document.getElementsByID("Desc-input"),
-  ImageInput: document.getElementsByID("Image-input"),
-  ColorInput: document.getElementsByID("Color-input"),
+  NameInput: document.getElementById("name-input"),
+  DescInput: document.getElementById("Desc-input"),
+  ImageInput: document.getElementById("Image-input"),
+  ColorInput: document.getElementById("Color-input"),
 };
 console.log(DOMSelectors.text);
-let id = 0
+let id = 0;
 
-function makecard(event) {
-  event.preventDefault();
+function makecard() {
   let name = DOMSelectors.NameInput.value;
   let desc = DOMSelectors.DescInput.value;
   let image = DOMSelectors.ImageInput.value;
   let color = DOMSelectors.ColorInput.value;
 
   DOMSelectors.container.insertAdjacentHTML(
-    id += 1,
     "beforeend",
+
     `
     <div class = "card" id = "${id}">
-    <h2 class = "card-header"> ${name} </h2>
-    <img src"${image}" alt="image made" class = "card-image">
-    <p>${desc}</p>
-
+      <h2 class = "card-header"> ${name} </h2>
+      <img src="${image}" alt="image made" class = "card-image">
+      <p>${desc}</p>
+    </div>
     
     `
   );
   let cardcolor = document.getElementById(id);
-  cardcolor.style.backgroundColor = color
+  cardcolor.style.backgroundColor = color;
 }
 
-function remove(event){
+function remove(event) {
   event.preventDefault();
   document.querySelector(".card").remove();
 }
@@ -49,17 +47,11 @@ function remove(event){
   console.log(event.target.parentElement);
   event.target.parentElement.style.backgroundColor = "red";
 });*/
-
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(document.querySelector("input").value);
+  makecard();
 });
-
-DOMSelectors.form.addEventListener("submit", function (event){
-  event.preventDefault();
-  makecard(event);
-})
-
 /*function backgroundandtext(background, text) {
   background.style.backgroundColor = "blue";
   text.style.fontSize = "50px";
